541554e3969e29486b63bd4338bc2cbb29408b52
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-05-03 23:33:24 -0400

    Fixed stats and routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const db = require("../models");
+const db = require("./models");
 
 const PORT = process.env.PORT || 8080;
 
